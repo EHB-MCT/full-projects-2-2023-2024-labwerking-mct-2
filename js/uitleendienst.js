d758707d9e9794e1542155db26683576ad9bc3c6
Dave Leroy <dave.leroy@student.ehb.be>
2024-06-14 16:06:36 +0200

    Fetching from API gives results in console log(Only first page)

--- js/uitleendienst.js
+++ js/uitleendienst.js
@@ -1,9 +1,27 @@
-document.addEventListener("DOMContentLoaded", function () {
-	document.querySelector("#submit").addEventListener("click", function () {
-		console.log("hey");
-		document.querySelector("#fase-1").classList.add("none");
-		document.querySelector("#fase-1").classList.remove("display");
-		document.querySelector("#fase-2").classList.add("display");
-		document.querySelector("#fase-2").classList.remove("none");
+function ini() {
+	nextPage();
+	fetchList();
+}
+function fetchList() {
+	fetch("https://labbxl.pockethost.io/api/collections/Items/records")
+		.then(function (response) {
+			return response.json();
+		})
+		.then(function (data) {
+			console.log(data);
+		});
+}
+
+function nextPage() {
+	document.addEventListener("DOMContentLoaded", function () {
+		document.querySelector("#submit").addEventListener("click", function () {
+			console.log("hey");
+			document.querySelector("#fase-1").classList.add("none");
+			document.querySelector("#fase-1").classList.remove("display");
+			document.querySelector("#fase-2").classList.add("display");
+			document.querySelector("#fase-2").classList.remove("none");
+		});
 	});
-});
+}
+
+ini();
