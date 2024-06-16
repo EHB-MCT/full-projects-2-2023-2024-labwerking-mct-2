891eb5ac3437e9adad606409b241ce1650c1c7e8
Seppe Ghekiere <seppe.ghekiere@student.ehb.be>
2024-06-16 15:37:24 +0200

    fixed nav mobile

--- js/nav.js
+++ js/nav.js
@@ -1,16 +1,22 @@
 document.addEventListener("DOMContentLoaded", function () {
+	let menu = false;
 	document.querySelector("#icon").addEventListener("click", function () {
-		document.querySelector("#menu").classList.add("visible");
-		document.querySelector("#menu").classList.remove("hidden");
-		document.querySelector("#navigation").classList.add("hidden");
-		document.querySelector("#navigation").classList.remove("visible");
-	});
-	document.querySelector("#icon2").addEventListener("click", function () {
-		document.querySelector("#menu").classList.add("hidden");
-		document.querySelector("#menu").classList.remove("visible");
-		document.querySelector("#navigation").classList.add("visible");
-		document.querySelector("#navigation").classList.remove("ghidden");
-
-		
+		if (menu == false) {
+			document.querySelector("#menu").classList.add("visible");
+			document.querySelector("#menu").classList.remove("hidden");
+			document.querySelector("#cross").classList.add("visible");
+			document.querySelector("#cross").classList.remove("hidden");
+			document.querySelector("#bars").classList.add("hidden");
+			document.querySelector("#bars").classList.remove("visible");
+			menu = true;
+		} else if (menu == true) {
+			document.querySelector("#menu").classList.add("hidden");
+			document.querySelector("#menu").classList.remove("visible");
+			document.querySelector("#bars").classList.add("visible");
+			document.querySelector("#bars").classList.remove("ghidden");
+			document.querySelector("#cross").classList.add("hidden");
+			document.querySelector("#cross").classList.remove("visible");
+			menu = false;
+		}
 	});
 });
