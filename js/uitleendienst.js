739f035d6e972e13d1734a9251cfa8bb74459cbf
Dave Leroy <dave.leroy@student.ehb.be>
2024-06-16 12:08:32 +0200

    Pulled out all items into the box, Picture don't show

--- js/uitleendienst.js
+++ js/uitleendienst.js
@@ -8,7 +8,39 @@ function fetchList() {
 			return response.json();
 		})
 		.then(function (data) {
+			document.querySelector(".catalog").innerHTML = "";
+			
+
 			console.log(data);
+
+			data.items.forEach(function(item) {
+				let newHtmlString = "";
+				console.log(item);
+				newHtmlString = 
+				`			<div class="catalog-item flex color3-border">
+								<img src="${item.Img}" class="catalog-item-image" alt="" />
+								<div class="flex-column catalog-item-info space-between">
+									<p class="font1 size2">${item.Naam}</p>
+									<p class="font1 size4">${item.Soort}</p>
+									<div class="flex item-info space-between">
+										<form action="#">
+											<select id="item-amount" class="item-amount font1 size4 color5-border color4-bg">				
+				`;
+	for(let i=0;i<item.InStock;i++){
+		newHtmlString += `					<option value="${i}" class="font1">${i}</option>`
+					}
+		newHtmlString += `
+											</select>
+										</form>
+										<button class="font1 size4 btn-item">Voeg toe</button>
+									</div>
+								</div>
+							</div>`
+				
+				
+
+				document.querySelector(".catalog").innerHTML += newHtmlString;
+			});
 		});
 }
 
