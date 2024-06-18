f88811823c393f4842613e9f2b71da9671ccfe5d
Dave Leroy <dave.leroy@student.ehb.be>
2024-06-18 10:19:16 +0200

    Being able to remove and add items to cart, (made local array so dont need to fetch everytime something gets added or removed

--- js/uitleendienst.js
+++ js/uitleendienst.js
@@ -1,5 +1,6 @@
 let statusFilter = "default";
 let basket = [];
+let catalogus = [];
 
 function ini() {
 	fetchList();
@@ -33,17 +34,22 @@ function filterSelection() {
 		});
 	});
 }
-function addItem() {
+function addRemoveItem() {
 	const buttons = document.querySelectorAll(".btn-item");
 	buttons.forEach((button) => {
 		button.addEventListener("click", function () {
 			console.log(`Button id ${this.id} `);
-			basket.push(this.id);
+			if (basket.indexOf(this.id) === -1) {
+				basket.push(this.id);
+			} else {
+				basket.splice(basket.indexOf(this.id), 1);
+			}
 			console.log(basket);
-			fetchList();
+			renderList(catalogus);
 		});
 	});
 }
+function removeItem() {}
 function fetchList() {
 	fetch("https://labbxl.pockethost.io/api/collections/Items/records/?perPage=200")
 		.then(function (response) {
@@ -52,155 +58,154 @@ function fetchList() {
 		.then(function (data) {
 			document.querySelector(".catalog").innerHTML = "";
 			console.log(data);
-			if (statusFilter === "LENS") {
-				data.items.forEach(function (item) {
-					if (item.Soort === "LENS") {
-						let newHtmlString = "";
-
-						newHtmlString = `			<div class="catalog-item flex color3-border">
-									<img src="${item.Img}" class="catalog-item-image" alt="" />
-									<div class="flex-column catalog-item-info space-between">
-										<p class="font1 size2">${item.Naam}</p>
-										<p class="font1 size4">${item.Soort}</p>
-										<div class="flex item-info space-between">
-											<form action="#">
-												<select id="item-amount" class="item-amount font1 size4 color5-border color4-bg">
-												<option value="0" class="font1">0</option>		
-					`;
-
-						for (let i = 0; i < item.InStock; i++) {
-							newHtmlString += `					<option value="${i + 1}" class="font1">${i + 1}</option>`;
-						}
-						newHtmlString += `
-												</select>
-											</form>
-											<button class="font1 size4 btn-item" id="${item.id}">Voeg toe</button>
-										</div>
-									</div>
-								</div>`;
-
-						document.querySelector(".catalog").innerHTML += newHtmlString;
-					}
-				});
-			} else if (statusFilter === "CAMERA") {
-				data.items.forEach(function (item) {
-					if (item.Soort === "CAMERA") {
-						let newHtmlString = "";
-
-						newHtmlString = `			<div class="catalog-item flex color3-border">
-									<img src="${item.Img}" class="catalog-item-image" alt="" />
-									<div class="flex-column catalog-item-info space-between">
-										<p class="font1 size2">${item.Naam}</p>
-										<p class="font1 size4">${item.Soort}</p>
-										<div class="flex item-info space-between">
-											<form action="#">
-												<select id="item-amount" class="item-amount font1 size4 color5-border color4-bg">
-												<option value="0" class="font1">0</option>		
-					`;
-
-						for (let i = 0; i < item.InStock; i++) {
-							newHtmlString += `					<option value="${i + 1}" class="font1">${i + 1}</option>`;
-						}
-						newHtmlString += `
-												</select>
-											</form>
-											<button class="font1 size4 btn-item" id="${item.id}">Voeg toe</button>
-										</div>
-									</div>
-								</div>`;
-
-						document.querySelector(".catalog").innerHTML += newHtmlString;
-					}
-				});
-			} else if (statusFilter === "MICROFOON") {
-				data.items.forEach(function (item) {
-					if (item.Soort === "MICROFOON") {
-						let newHtmlString = "";
-
-						newHtmlString = `			<div class="catalog-item flex color3-border">
-									<img src="${item.Img}" class="catalog-item-image" alt="" />
-									<div class="flex-column catalog-item-info space-between">
-										<p class="font1 size2">${item.Naam}</p>
-										<p class="font1 size4">${item.Soort}</p>
-										<div class="flex item-info space-between">
-											<form action="#">
-												<select id="item-amount" class="item-amount font1 size4 color5-border color4-bg">
-												<option value="0" class="font1">0</option>		
-					`;
-
-						for (let i = 0; i < item.InStock; i++) {
-							newHtmlString += `					<option value="${i + 1}" class="font1">${i + 1}</option>`;
-						}
-						newHtmlString += `
-												</select>
-											</form>
-											<button class="font1 size4 btn-item" id="${item.id}">Voeg toe</button>
-										</div>
-									</div>
-								</div>`;
-
-						document.querySelector(".catalog").innerHTML += newHtmlString;
-					}
-				});
-			} else {
-				document.querySelector(".materials").innerHTML = "";
-				data.items.forEach(function (item) {
-					let newHtmlString = "";
-					if (basket.indexOf(item.id) !== -1) {
-						newHtmlString = `<div class="catalog-item flex color3-border">
-									<img src="${item.Img}" class="catalog-item-image" alt="" />
-									<div class="flex-column catalog-item-info space-between">
-										<p class="font1 size2">${item.Naam}</p>
-										<p class="font1 size4">${item.Soort}</p>
-										<div class="flex item-info space-between">
-											<form action="#">
-												<select id="item-amount" class="item-amount font1 size4 color5-border color4-bg">
-												<option value="0" class="font1">0</option>		
-					`;
-
-						for (let i = 0; i < item.InStock; i++) {
-							newHtmlString += `					<option value="${i + 1}" class="font1">${i + 1}</option>`;
-						}
-						newHtmlString += `
-												</select>
-											</form>
-											<button class="font1 size4 btn-item" id="${item.id}">Voeg toe</button>
-										</div>
-									</div>
-								</div>`;
-
-						document.querySelector(".materials").innerHTML += newHtmlString;
-					} else {
-						newHtmlString = `<div class="catalog-item flex color3-border">
-									<img src="${item.Img}" class="catalog-item-image" alt="" />
-									<div class="flex-column catalog-item-info space-between">
-										<p class="font1 size2">${item.Naam}</p>
-										<p class="font1 size4">${item.Soort}</p>
-										<div class="flex item-info space-between">
-											<form action="#">
-												<select id="item-amount" class="item-amount font1 size4 color5-border color4-bg">
-												<option value="0" class="font1">0</option>		
-					`;
-
-						for (let i = 0; i < item.InStock; i++) {
-							newHtmlString += `					<option value="${i + 1}" class="font1">${i + 1}</option>`;
-						}
-						newHtmlString += `
-												</select>
-											</form>
-											<button class="font1 size4 btn-item" id="${item.id}">Voeg toe</button>
-										</div>
-									</div>
-								</div>`;
-
-						document.querySelector(".catalog").innerHTML += newHtmlString;
-					}
-				});
-			}
-		})
-		.then(function () {
-			addItem();
+			catalogus = data;
+			renderList(catalogus);
 		});
+}
+function renderList(catalogus) {
+	if (statusFilter === "LENS") {
+		catalogus.items.forEach(function (item) {
+			if (item.Soort === "LENS") {
+				let newHtmlString = "";
+				newHtmlString = `<div class="catalog-item flex color3-border">
+							<img src="${item.Img}" class="catalog-item-image" alt="" />
+							<div class="flex-column catalog-item-info space-between">
+								<p class="font1 size2">${item.Naam}</p>
+								<p class="font1 size4">${item.Soort}</p>
+								<div class="flex item-info space-between">
+									<form action="#">
+										<select id="item-amount" class="item-amount font1 size4 color5-border color4-bg">
+										<option value="0" class="font1">0</option>		
+			`;
+				for (let i = 0; i < item.InStock; i++) {
+					newHtmlString += `					<option value="${i + 1}" class="font1">${i + 1}</option>`;
+				}
+				newHtmlString += `
+										</select>
+									</form>
+									<button class="font1 size4 btn-item" id="${item.id}">Voeg toe</button>
+								</div>
+							</div>
+						</div>`;
+				document.querySelector(".catalog").innerHTML += newHtmlString;
+			}
+		});
+	} else if (statusFilter === "CAMERA") {
+		data.items.forEach(function (item) {
+			if (item.Soort === "CAMERA") {
+				let newHtmlString = "";
+
+				newHtmlString = `			<div class="catalog-item flex color3-border">
+							<img src="${item.Img}" class="catalog-item-image" alt="" />
+							<div class="flex-column catalog-item-info space-between">
+								<p class="font1 size2">${item.Naam}</p>
+								<p class="font1 size4">${item.Soort}</p>
+								<div class="flex item-info space-between">
+									<form action="#">
+										<select id="item-amount" class="item-amount font1 size4 color5-border color4-bg">
+										<option value="0" class="font1">0</option>		
+			`;
+
+				for (let i = 0; i < item.InStock; i++) {
+					newHtmlString += `					<option value="${i + 1}" class="font1">${i + 1}</option>`;
+				}
+				newHtmlString += `
+										</select>
+									</form>
+									<button class="font1 size4 btn-item" id="${item.id}">Voeg toe</button>
+								</div>
+							</div>
+						</div>`;
+
+				document.querySelector(".catalog").innerHTML += newHtmlString;
+			}
+		});
+	} else if (statusFilter === "MICROFOON") {
+		data.items.forEach(function (item) {
+			if (item.Soort === "MICROFOON") {
+				let newHtmlString = "";
+
+				newHtmlString = `			<div class="catalog-item flex color3-border">
+							<img src="${item.Img}" class="catalog-item-image" alt="" />
+							<div class="flex-column catalog-item-info space-between">
+								<p class="font1 size2">${item.Naam}</p>
+								<p class="font1 size4">${item.Soort}</p>
+								<div class="flex item-info space-between">
+									<form action="#">
+										<select id="item-amount" class="item-amount font1 size4 color5-border color4-bg">
+										<option value="0" class="font1">0</option>		
+			`;
+
+				for (let i = 0; i < item.InStock; i++) {
+					newHtmlString += `					<option value="${i + 1}" class="font1">${i + 1}</option>`;
+				}
+				newHtmlString += `
+										</select>
+									</form>
+									<button class="font1 size4 btn-item" id="${item.id}">Voeg toe</button>
+								</div>
+							</div>
+						</div>`;
+
+				document.querySelector(".catalog").innerHTML += newHtmlString;
+			}
+		});
+	} else {
+		document.querySelector(".materials").innerHTML = "";
+		catalogus.items.forEach(function (item) {
+			let newHtmlString = "";
+			if (basket.indexOf(item.id) !== -1) {
+				newHtmlString = `<div class="catalog-item flex color3-border">
+							<img src="${item.Img}" class="catalog-item-image" alt="" />
+							<div class="flex-column catalog-item-info space-between">
+								<p class="font1 size2">${item.Naam}</p>
+								<p class="font1 size4">${item.Soort}</p>
+								<div class="flex item-info space-between">
+									<form action="#">
+										<select id="item-amount" class="item-amount font1 size4 color5-border color4-bg">
+										<option value="0" class="font1">0</option>		
+			`;
+
+				for (let i = 0; i < item.InStock; i++) {
+					newHtmlString += `					<option value="${i + 1}" class="font1">${i + 1}</option>`;
+				}
+				newHtmlString += `
+										</select>
+									</form>
+									<button class="font1 size4 btn-item" id="${item.id}">Verwijder</button>
+								</div>
+							</div>
+						</div>`;
+
+				document.querySelector(".materials").innerHTML += newHtmlString;
+			} else {
+				newHtmlString = `<div class="catalog-item flex color3-border">
+							<img src="${item.Img}" class="catalog-item-image" alt="" />
+							<div class="flex-column catalog-item-info space-between">
+								<p class="font1 size2">${item.Naam}</p>
+								<p class="font1 size4">${item.Soort}</p>
+								<div class="flex item-info space-between">
+									<form action="#">
+										<select id="item-amount" class="item-amount font1 size4 color5-border color4-bg">
+										<option value="0" class="font1">0</option>		
+			`;
+
+				for (let i = 0; i < item.InStock; i++) {
+					newHtmlString += `					<option value="${i + 1}" class="font1">${i + 1}</option>`;
+				}
+				newHtmlString += `
+										</select>
+									</form>
+									<button class="font1 size4 btn-item" id="${item.id}">Voeg toe</button>
+								</div>
+							</div>
+						</div>`;
+
+				document.querySelector(".catalog").innerHTML += newHtmlString;
+			}
+		});
+	}
+	addRemoveItem();
 }
 function nextPage() {
 	document.addEventListener("DOMContentLoaded", function () {
