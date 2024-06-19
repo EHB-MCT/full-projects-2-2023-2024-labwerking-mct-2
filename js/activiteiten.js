0905667701f2da7d77861738384b7bf19b3367e6
Pema <pema.bhutia@student.ehb.be>
2024-06-19 14:26:37 +0200

    activiteiten responsive

--- js/activiteiten.js
+++ js/activiteiten.js
@@ -1,13 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
 	let acc = document.getElementsByClassName("accordion");
+
 	for (let i = 0; i < acc.length; i++) {
 		acc[i].addEventListener("click", function () {
 			this.classList.toggle("active");
 			let panel = this.nextElementSibling;
+			let arrow = this.querySelector(".arrow");
+
 			if (panel.style.display === "block") {
 				panel.style.display = "none";
+				arrow.style.transform = "rotate(0deg)";
 			} else {
 				panel.style.display = "block";
+				arrow.style.transform = "rotate(180deg)";
 			}
 		});
 	}
