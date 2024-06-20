ff27c44db323e2aeae0c508bc5e446ee5e783655
Seppe Ghekiere <seppe.ghekiere@student.ehb.be>
2024-06-20 14:12:48 +0200

    maybe carousel mobile homepzge works

--- js/index.js
+++ js/index.js
@@ -5,18 +5,34 @@ document.addEventListener("DOMContentLoaded", function () {
 	function showSlides() {
 		for (let i = 0; i < slides.length; i++) {
 			slides[i].style.zIndex = -1;
-			if (i === slideIndex) {
-				slides[i].style.transform = "translateX(0%) rotateY(0deg) scale(1.5)";
-				slides[i].style.zIndex = 2;
-				slides[i].style.textAlign = "center";
-			} else if (i === (slideIndex + 1) % slides.length) {
-				slides[i].style.transform = "translateX(-15vw) translateY(-7.5%) rotateY(0deg) scale(1)";
-				slides[i].style.zIndex = 0;
-				slides[i].style.textAlign = i === (slideIndex + 1) % slides.length ? "left" : "right";
-			} else if (i === (slideIndex + 2) % slides.length) {
-				slides[i].style.transform = "translateX(15vw) translateY(-7.5%) rotateY(0deg) scale(1)";
-				slides[i].style.zIndex = 1;
-				slides[i].style.textAlign = i === (slideIndex + 2) % slides.length ? "right" : "left";
+			if (/Mobi/i.test(navigator.userAgent)) {
+				if (i === slideIndex) {
+					slides[i].style.transform = "translateX(0%) rotateY(0deg) scale(1.5)";
+					slides[i].style.zIndex = 2;
+					slides[i].style.textAlign = "center";
+				} else if (i === (slideIndex + 1) % slides.length) {
+					slides[i].style.transform = "translateX(-27.5vw) translateY(-7.5%) rotateY(0deg) scale(1)";
+					slides[i].style.zIndex = 0;
+					slides[i].style.textAlign = i === (slideIndex + 1) % slides.length ? "left" : "right";
+				} else if (i === (slideIndex + 2) % slides.length) {
+					slides[i].style.transform = "translateX(27.5vw) translateY(-7.5%) rotateY(0deg) scale(1)";
+					slides[i].style.zIndex = 1;
+					slides[i].style.textAlign = i === (slideIndex + 2) % slides.length ? "right" : "left";
+				}
+			} else {
+				if (i === slideIndex) {
+					slides[i].style.transform = "translateX(0%) rotateY(0deg) scale(1.5)";
+					slides[i].style.zIndex = 2;
+					slides[i].style.textAlign = "center";
+				} else if (i === (slideIndex + 1) % slides.length) {
+					slides[i].style.transform = "translateX(-15vw) translateY(-7.5%) rotateY(0deg) scale(1)";
+					slides[i].style.zIndex = 0;
+					slides[i].style.textAlign = i === (slideIndex + 1) % slides.length ? "left" : "right";
+				} else if (i === (slideIndex + 2) % slides.length) {
+					slides[i].style.transform = "translateX(15vw) translateY(-7.5%) rotateY(0deg) scale(1)";
+					slides[i].style.zIndex = 1;
+					slides[i].style.textAlign = i === (slideIndex + 2) % slides.length ? "right" : "left";
+				}
 			}
 		}
 		slideIndex = (slideIndex + 1) % slides.length;
